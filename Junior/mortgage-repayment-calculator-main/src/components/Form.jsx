cd5241a7dc1f1ed1469e133f006693b6f73d0a73
Wang-yin <sky06456@gmail.com>
2025-03-19 03:05:53 +0800

    feat: complete layout and responsive design for mortgage repayment calculator
    - Implemented the basic layout structure for the mortgage repayment calculator
    - Added responsive design to ensure proper display on various screen sizes
    - TODO: Implement active states for interactive elements

--- Junior/mortgage-repayment-calculator-main/src/components/Form.jsx
+++ Junior/mortgage-repayment-calculator-main/src/components/Form.jsx
@@ -1,31 +1,41 @@
-import "../styles/Form.css"
+import "../styles/Form.css";
 
-function Form () {
+function Form() {
   return (
     <form className="flex-column form">
       <div className="header">
         <h2 className="form-title">Mortgage Calculator</h2>
-        <button type="reset">Clear All</button>
+        <button type="reset" id="clear-all">
+          Clear All
+        </button>
       </div>
       <div className="form-control flex-column">
-        <label htmlFor="amount" className="mb">Mortgage Amount</label>
+        <label htmlFor="amount" className="mb">
+          Mortgage Amount
+        </label>
         <div className="flex frame">
           <p className="unit">$</p>
           <input id="amount" type="text"></input>
         </div>
       </div>
-      <div className="form-control flex-column">
-        <label htmlFor="term" className="mb">Mortgage Term</label>
-        <div className="flex frame">
-          <input id="term" type="text"></input>
-          <p className="unit">years</p>
+      <div className="term-rate">
+        <div className="form-control flex-column">
+          <label htmlFor="term" className="mb">
+            Mortgage Term
+          </label>
+          <div className="flex frame">
+            <input id="term" type="text"></input>
+            <p className="unit">years</p>
+          </div>
         </div>
-      </div>
-      <div className="form-control flex-column">
-        <label htmlFor="rate" className="mb">Interest Rate</label>
-        <div className="flex frame">
-          <input id="rate" type="text"></input>
-          <p className="unit">%</p>
+        <div className="form-control flex-column">
+          <label htmlFor="rate" className="mb">
+            Interest Rate
+          </label>
+          <div className="flex frame">
+            <input id="rate" type="text"></input>
+            <p className="unit">%</p>
+          </div>
         </div>
       </div>
       <fieldset>
@@ -39,9 +49,12 @@ function Form () {
           <label htmlFor="Interest">Interest Only</label>
         </div>
       </fieldset>
-      <button><img src="/images/icon-calculator.svg" alt="calculator"></img>Calculate Repayments</button>
+      <button type="submit" className="button-submit">
+        <img src="/images/icon-calculator.svg" alt="calculator"></img>
+        <span>Calculate Repayments</span>
+      </button>
     </form>
-  )
+  );
 }
 
-export default Form
+export default Form;
